062fb7cee508e95939928810eb4429dcda421148
Juan M. Bello-Rivas <jbellorivas@rigetti.com>
2020-03-13 17:34:57 -0700

    Address reviews.

--- app/src/debugger.lisp
+++ app/src/debugger.lisp
@@ -16,6 +16,10 @@
 
 (defparameter *debugger-prompt* "(qvm-debugger) ")
 
+(defun error-unless-qvm-exists ()
+  (unless *qvm*
+    (error "No program has been loaded.")))
+
 (defstruct command
   (name nil :type string)
   (function nil :type function)
@@ -25,13 +29,20 @@
   "Association list of available commands.")
 
 (defmacro define-debugger-command (name lambda-list &body body)
-  "Define a new command function named %NAME (note that the % character is prepended to NAME) with the specified LAMBDA-LIST and BODY."
+  "Define a new command function named %NAME (note that the % character is prepended to NAME) with the specified LAMBDA-LIST and BODY.
+
+Documentation strings are mandatory in debugger commands and they should be formatted as follows:
+
+1. First line containing usage information.
+2. Empty line.
+3. Longer description in a line by itself.
+4. Optional additional documentation."
   (unless (stringp (first body))
     (error "Documentation string missing."))
   (let ((documentation (pop body))
         (lowercase-name (string-downcase name))
         (symbol (intern (concatenate 'string "%" (symbol-name name)))))
-    `(progn
+    `(prog1
        (defun ,symbol ,lambda-list
          ,@body)
 
@@ -41,13 +52,8 @@
                                              :documentation ,documentation)
                                *commands*)))))
 
-(defun string-prefix-p (a b)
-  (string= a b :end2 (min (length a) (length b))))
-
 (defun %get-command (command-name)
-  (let ((match (assoc command-name *commands* :test #'string-prefix-p)))
-    (when match
-      (rest match))))
+  (alexandria:assoc-value *commands* command-name :test #'uiop:string-prefix-p))
 
 (defun get-command (command-name)
   "Return COMMAND structure corresponding to COMMAND-NAME. Signal an error if no such command exists."
@@ -61,12 +67,17 @@
 
 (defun eval-command (command-name &optional args)
   (let ((function (command-function (get-command command-name))))
-    (if (or (null args) (string= args ""))
-        (funcall function)
-        (funcall function args))))
+    (if (and args (string/= args ""))
+        (funcall function args)
+        (funcall function))))
 
 (defun command-description (command)
-  (aref (nth-value 1 (cl-ppcre:scan-to-strings ".*\\n\\n(.*)" (command-documentation command))) 0))
+  "Return command description string found in the second non-empty line of the docstring corresponding to COMMAND."
+  (let* ((documentation (command-documentation command))
+         (substrings (nth-value 1 (cl-ppcre:scan-to-strings ".*\\n\\w*\\n(.*)" documentation))))
+    (unless (= (length substrings) 1)
+      (error "Invalid documentation string for ~S." (command-name command)))
+    (aref substrings 0)))
 
 (define-debugger-command help (&optional command-name)
   "Usage: help [COMMAND]
@@ -81,21 +92,24 @@ Display help message."
   "Usage: reset
 
 Reset the current QVM to the zero ket and set the program counter to zero."
-  (when *qvm*
-    (qvm::reset-quantum-state *qvm*)
-    (qvm::reset-classical-memory *qvm*)
-    (setf (qvm::pc *qvm*) 0)))
+  (error-unless-qvm-exists)
+  (qvm::reset-quantum-state *qvm*)
+  (qvm::reset-classical-memory *qvm*)
+  (setf (qvm::pc *qvm*) 0))
 
 (define-debugger-command list ()
   "Usage: list
 
 Disassemble (list) the currently loaded program."
-  (unless *qvm*
-    (error "No program has been loaded."))
-  (loop :for instruction-number :from 0
-        :for instruction :across (qvm::program *qvm*) :do
-          (format t "~&~D~10T~/cl-quil:instruction-fmt/~%"
-                  instruction-number instruction)))
+  (error-unless-qvm-exists)
+  (flet ((get-marker-index (instruction-index)
+           (if *qvm*
+               (boolean-bit (= instruction-index (qvm::pc *qvm*)))
+               0)))
+    (loop :for instruction-index :from 0
+          :for instruction :across (qvm::program *qvm*) :do
+            (format t "~&~[ ~;*~]~D~10T~/cl-quil:instruction-fmt/~%"
+                    (get-marker-index instruction-index) instruction-index instruction))))
 
 (define-debugger-command run ()
   "Usage: run
@@ -103,13 +117,10 @@ Disassemble (list) the currently loaded program."
 Run the current program from the beginning."
   (cond
     ((and *qvm* (plusp (length (qvm::program *qvm*))))
-     (%reset)
-     (setf (qvm::pc *qvm*) 0)
-     (loop :for pc := (qvm::pc *qvm*)
+     (loop :initially (%reset)
+           :for pc := (qvm::pc *qvm*)
            :for breakpoint-p := (find pc *breakpoints*)
-           :until (or (null pc)
-                      (>= pc (qvm::loaded-program-length *qvm*))
-                      breakpoint-p)
+           :until (or (program-finished-p) breakpoint-p)
            :do (setf *qvm* (qvm:transition *qvm* (qvm::current-instruction *qvm*)))
            :finally (when breakpoint-p
                       (format t "Stopped at breakpoint before instruction:~%~
@@ -117,68 +128,49 @@ Run the current program from the beginning."
                               pc (qvm::current-instruction *qvm*)))
                     (return *qvm*))
      (when *display*
-       (%display-registers)))
+       (display-amplitudes)))
     (t
      (error "No program has been loaded."))))
 
-(define-debugger-command display-registers ()
-  "Usage: display-registers
-
-Show the contents of the wavefunction. You can enable running this command after every step by running the \"display\" command."
-  (format t "Amplitudes:~%")
-  (let ((nq (qvm:number-of-qubits *qvm*)))
-    (qvm:map-amplitudes
-     *qvm*
-     (let ((i 0))
-       (lambda (z)
-         (let ((p (* 100 (qvm:probability z))))
-           (when (> p *threshold*)
-             (format t
-                     "|~v,'0B>: ~/qvm-app::pprint-complex/, ~64TP=~5F~%"
-                     nq
-                     i
-                     z
-                     p)))
-         (incf i))))))
-
-(defun program-finished-p (qvm)
-  (or (null (qvm::pc qvm))
-      (>= (qvm::pc qvm) (qvm::loaded-program-length qvm))))
+(defun program-finished-p ()
+  (or (null (qvm::pc *qvm*))
+      (>= (qvm::pc *qvm*)
+          (qvm::loaded-program-length *qvm*))))
 
 (define-debugger-command step ()
   "Usage: step
 
 Run the next instruction and stop."
-  (when *qvm*
-    (cond
-      ((program-finished-p *qvm*)
-       (format t "Finished program execution.~%"))
-      (t
-       (quil::print-instruction (qvm::current-instruction *qvm*))
-       (terpri)
-       (setf *qvm* (qvm:transition *qvm* (qvm::current-instruction *qvm*)))
-       (when *display*
-         (%display-registers))))))
+  (error-unless-qvm-exists)
+  (cond
+    ((program-finished-p)
+     (format t "Finished program execution.~%"))
+    (t
+     (quil::print-instruction (qvm::current-instruction *qvm*))
+     (terpri)
+     (setf *qvm* (qvm:transition *qvm* (qvm::current-instruction *qvm*)))
+     (when *display*
+       (display-amplitudes)))))
 
 (define-debugger-command continue ()
   "Usage: continue
 
 Resume program execution from the current instruction."
-  (when *qvm*
-    (cond
-      ((program-finished-p *qvm*)
-       (format t "Finished program execution.~%"))
-      (t
-       (loop :for pc := (qvm::pc *qvm*)
-             :for counter := 0
-             :for breakpoint-p := (and (find pc *breakpoints*)
-                                       (plusp counter))
-             :until (or (program-finished-p *qvm*) breakpoint-p) :do
-               (setf *qvm* (qvm:transition *qvm* (qvm::current-instruction *qvm*)))
-             :finally (when breakpoint-p
-                        (format t "Stopping at breakpoint in instruction ~D:~%~/quil:instruction-fmt/~%"
-                                pc (qvm::current-instruction *qvm*)))
-                      (return *qvm*))))))
+  (error-unless-qvm-exists)
+  (cond
+    ((program-finished-p)
+     (format t "Finished program execution.~%"))
+    (t
+     (loop :for pc := (qvm::pc *qvm*)
+           :for counter :from 0
+           :for breakpoint-p := (and (find pc *breakpoints*)
+                                     (plusp counter))
+           :until (or (program-finished-p) breakpoint-p) :do
+             (setf *qvm* (qvm:transition *qvm* (qvm::current-instruction *qvm*)))
+           :finally (when breakpoint-p
+                      (format t "Stopping at breakpoint in instruction ~D:~%~/quil:instruction-fmt/~%"
+                              pc (qvm::current-instruction *qvm*)))
+                    (return *qvm*)))))
 
 (define-debugger-command load (filename)
   "Usage: load FILENAME
@@ -186,7 +178,7 @@ Resume program execution from the current instruction."
 Load a program instantiating a suitable QVM."
   (unless filename
     (error "File name not specified."))
-  (let* ((program (quil:read-quil-file filename))
+  (let* ((program (quil:read-quil-file (string-trim " " filename)))
          (number-of-qubits (quil:qubits-needed program)))
     (format t "Read ~A using ~D qubits.~%" filename number-of-qubits)
     (setf *source-filename* filename
@@ -210,19 +202,34 @@ Re-read the file corresponding to the previously loaded program."
   "Usage: break INSTRUCTION-INDEX
 
 Set up a breakpoint at the specified instruction."
-  (when *qvm*
-    (let ((idx (parse-integer instruction-index)))
-      (unless (and (integerp idx)
-                   (not (minusp idx))
-                   (< idx (length (qvm::program *qvm*))))
-        (error "Invalid instruction index ~S." idx))
-      (pushnew idx *breakpoints*)
-      (setf *breakpoints* (sort *breakpoints* #'<)))))
+  (error-unless-qvm-exists)
+  (let ((idx (parse-integer instruction-index)))
+    (unless (and (integerp idx)
+                 (not (minusp idx))
+                 (< idx (length (qvm::program *qvm*))))
+      (error "Invalid instruction index ~S." idx))
+    (pushnew idx *breakpoints*)
+    (setf *breakpoints* (sort *breakpoints* #'<))))
+
+(defun display-amplitudes ()
+  (format t "Amplitudes:~%")
+  (let ((nq (qvm:number-of-qubits *qvm*)))
+    (qvm:map-amplitudes
+     *qvm*
+     (let ((i 0))
+       (lambda (z)
+         (let ((p (* 100 (qvm:probability z))))
+           (when (> p *threshold*)
+             (format t
+                     "|~v,'0B>: ~/qvm-app::pprint-complex/, ~64TP=~5F~%"
+                     nq i z p)))
+         (incf i))))))
 
 (define-debugger-command display ()
   "Usage: display
 
-Enable register display."
+Show the contents of the wavefunction. You can enable running this command after every step by running the \"display\" command."
+  (error-unless-qvm-exists)
   (unless *display*
     (format t "Enabling register display.~%")
     (setf *display* t)))
@@ -231,6 +238,7 @@ Enable register display."
   "Usage: delete
 
 Delete breakpoints and disable register display."
+  (error-unless-qvm-exists)
   (setf *display* nil
         *breakpoints* nil))
 
@@ -243,23 +251,23 @@ Delete breakpoints and disable register display."
             (subseq string (aref reg-start 1) (aref reg-end 1))))))
 
 (define-debugger-command info (args)
-  "Usage: info COMMAND where COMMAND is 'qpu', 'registers', or 'classical'.
+  "Usage: info COMMAND where COMMAND is 'qvm', 'registers', or 'classical'.
 
 Show internal state information."
   (let ((subcommand (first (parse-string args))))
     (cond
-      ((string-prefix-p subcommand "qpu")
+      ((uiop:string-prefix-p subcommand "qvm")
        (if *qvm*
-           (format t "QPU is ~A with ~D qubits.~%" *qvm* (qvm:number-of-qubits *qvm*))
+           (format t "QVM is ~A with ~D qubits.~%" *qvm* (qvm:number-of-qubits *qvm*))
            (format t "No QVM has been instantiated.~%")))
-      ((string-prefix-p subcommand "registers")
+      ((uiop:string-prefix-p subcommand "registers")
        (when *qvm*
          (format t "Program counter: ~D~%" (qvm::pc *qvm*))
-         (%display-registers)))
-      ((string-prefix-p subcommand "classical")
+         (display-amplitudes)))
+      ((uiop:string-prefix-p subcommand "classical")
        (when *qvm*
          (qvm-app::print-classical-memory *qvm*)))
-      ((string-prefix-p subcommand "breakpoints")
+      ((uiop:string-prefix-p subcommand "breakpoints")
        (format t "~@(~R~) breakpoint~:P currently set.~%" (length *breakpoints*))
        (loop :for breakpoint :in *breakpoints* :do
          (format t "~&~D~10T~/cl-quil:instruction-fmt/~%" breakpoint (elt (qvm::program *qvm*) breakpoint))))
@@ -281,7 +289,7 @@ Show internal state information."
         :with previous-args := nil
         :for string := (formedit :prompt1 *debugger-prompt*)
         :for (command args) := (parse-string string)
-        :until (string-prefix-p command "quit")
+        :until (uiop:string-prefix-p command "quit")
         :if command :do
           (handler-case
               (progn
